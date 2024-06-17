a0bf424943567d94fb85a6fef31ae9b764c6edef
mark <mark@onpoint-holdings.com>
2024-06-18 01:22:53 +0800

    Crud for Account done.

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,7 +4,7 @@
 return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	'connectionString' => 'mysql:host=localhost;port=3302;dbname=elearning_db',
+	'connectionString' => 'mysql:host=localhost;port=3306;dbname=e_learning_db',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
